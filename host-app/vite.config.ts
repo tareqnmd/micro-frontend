8eb1f108a2ca809f39515901bf2bb41745022802
Tareq <md.tareq@asthait.com>
2025-09-21 09:38:49 +0600

    feat: add federation plugin configuration and build options in Vite config for host and remote applications

--- host-app/vite.config.ts
+++ host-app/vite.config.ts
@@ -1,8 +1,25 @@
+import federation from '@originjs/vite-plugin-federation';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		federation({
+			name: 'host_app',
+			remotes: {
+				remote_app: 'http://localhost:5001/assets/remoteEntry.js',
+			},
+			shared: ['react', 'react-dom'],
+		}),
+	],
+	build: {
+		modulePreload: false,
+		target: 'esnext',
+		minify: false,
+		cssCodeSplit: false,
+	},
 });
--- remote-app/vite.config.ts
+++ remote-app/vite.config.ts
@@ -1,8 +1,32 @@
+import federation from '@originjs/vite-plugin-federation';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		federation({
+			name: 'remote_app',
+			filename: 'remoteEntry.js',
+			exposes: {
+				'./Button': './src/components/Button',
+				'./Header': './src/components/Header',
+			},
+			shared: ['react', 'react-dom'],
+		}),
+	],
+	build: {
+		modulePreload: false,
+		target: 'esnext',
+		minify: false,
+		cssCodeSplit: false,
+	},
+	preview: {
+		port: 5001,
+		strictPort: true,
+		cors: true,
+	},
 });
